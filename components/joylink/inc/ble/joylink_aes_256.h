4ec080a3cc5ff4b3b3658d736168694edd72fe98
wuliang <baidu_studio@163.com>
2022-08-30 19:35:45 +0800

    [rm] Delete Unused code.

--- components/joylink/inc/ble/joylink_aes_256.h
+++
@@ -1,8 +0,0 @@
-#ifndef __JOYLINK_AES_256_H__
-#define __JOYLINK_AES_256_H__
-
-void key_expansion(const uint8_t *key, uint8_t *w);
-void cipher(uint8_t *in,  uint8_t *w);
-void inv_cipher(uint8_t *in, uint8_t *w);
-
-#endif
